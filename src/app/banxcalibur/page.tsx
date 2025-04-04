8849d1646e1c06dbb8579668c66857235c19751f
djblanc360 <djblanc360@gmail.com>
2025-04-03 20:15:04 -0700

    silly error in banxcalibur

--- src/app/banxcalibur/page.tsx
+++ src/app/banxcalibur/page.tsx
@@ -6,7 +6,7 @@ export default function BanxcliburPage() {
         <Navbar />
         <div className="min-h-screen flex items-center justify-center">
           <div className="text-center">
-            <h1 className="text-4xl font-bold mb-4 text-rose-300">Banxclibur<</h1>
+            <h1 className="text-4xl font-bold mb-4 text-rose-300">Banxclibur</h1>
             <p className="text-xl">Access exclusive drops and special offers..</p>
           </div>
         </div>
